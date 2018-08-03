5b0109972afc3d2c7a103445eb17501ac076e564
NotControl <18236556108@163.com>
2018-08-03 21:05:29 +0800

    Spring Cloud 服务提供者与消费者demo

--- cloud-demo-client-b/src/main/java/com/icourt/clouddemoclientb/controller/DemoController.java
+++ cloud-demo-client-b/src/main/java/com/icourt/clouddemoclientb/controller/DemoController.java
@@ -1,11 +1,8 @@
 package com.icourt.clouddemoclientb.controller;
 
 import com.icourt.common.entity.BaseUser;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
